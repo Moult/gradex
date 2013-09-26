bfdcddf172ee9531ca58cd0e19d63f851ecf73ba
Dion Moult <dion@thinkmoult.com>
2013-09-27 00:51:37 +1000

    Basic slice.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+$("section#showcase img").hide();
+function switchShowcase() {
+    $("section#showcase img").first().appendTo("section#showcase").fadeOut(3000);
+    $("section#showcase img").first().fadeIn(3000);
+    setTimeout(switchShowcase, 10000);
+}
+switchShowcase();
