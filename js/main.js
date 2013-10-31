659829d9e380845cbd8b6953a58fe73746dff97d
Dion Moult <dion@thinkmoult.com>
2013-11-01 01:09:20 +1100

    Bump with edits

--- js/main.js
+++ js/main.js
@@ -5,3 +5,40 @@ function switchShowcase() {
     setTimeout(switchShowcase, 10000);
 }
 switchShowcase();
+
+
+$("#viewDetails").click(function() {
+    if ($("#viewDetails").hasClass("visible")) {
+        $("#viewDetails").removeClass("visible");
+    } else {
+        $("#details").slideToggle('slow');
+        $("#viewDetails").addClass("visible");
+    }
+});
+
+
+$("body").mCustomScrollbar({
+    theme: "light",
+    scrollInertia: 100,
+    advanced:{
+        updateOnContentResize: true
+    }
+});
+
+$("h2.dropdown").click(function() {
+    if ($("ul.dropdown").hasClass("visible")) {
+        $("ul.dropdown").removeClass("visible");
+    } else {
+        $("ul.dropdown").slideToggle();
+        $("ul.dropdown").addClass("visible");
+    }
+});
+
+$("a.readmore").click(function() {
+    if ($("p.readmore").hasClass("visible")) {
+        $("p.readmore").removeClass("visible");
+    } else {
+        $("p.readmore").slideToggle();
+        $("p.readmore").addClass("visible");
+    }
+});
