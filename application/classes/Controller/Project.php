40fa3711e94f25284aee94370ddd3340def39a39
Dion Moult <dion@thinkmoult.com>
2013-11-10 15:40:49 +1100

    Add basic form names and validation (backend and html5)

--- application/classes/Controller/Project.php
+++ application/classes/Controller/Project.php
@@ -8,5 +8,75 @@ class Controller_Project extends Controller_Core
 {
     public function action_submit()
     {
+        if ($this->has_pressed_the_submit_button())
+        {
+            $session = Session::instance();
+            try
+            {
+                $this->get_image_add_usecase()->fetch()->interact();
+            }
+            catch (Cavis\Core\Exception\Validation $e)
+            {
+                $session->set('errors', $e->get_errors());
+                foreach ($this->request->post() as $key => $value)
+                {
+                    $this->view->$key = $value;
+                }
+            }
+        }
+    }
+
+    private function has_pressed_the_submit_button()
+    {
+        return (bool) ($this->request->method() === HTTP_Request::POST);
+    }
+
+    private function get_image_add_usecase()
+    {
+        $image = new Cavis\Core\Data\Image;
+        $supplementary_files = array();
+        $category = new Cavis\Core\Data\Category;
+
+        $image->name = $this->request->post('name');
+        $image->author = $this->request->post('author');
+        $image->website = $this->request->post('website');
+        $image->email = $this->request->post('email');
+        $image->summary = $this->request->post('summary');
+        $image->description = $this->request->post('description');
+
+        $category->id = $this->request->post('category');
+
+        $file = $this->extract_posted_file_into_file_data('file');
+        $supplementary_files[] = $this->extract_posted_file_into_file_data('supplementary_1');
+        $supplementary_files[] = $this->extract_posted_file_into_file_data('supplementary_2');
+        $supplementary_files[] = $this->extract_posted_file_into_file_data('supplementary_3');
+
+        $image->file = $file;
+        $image->supplementary_files = $supplementary_files;
+        $image->category = $category;
+
+        return new Cavis\Core\Usecase\Image\Add(
+            array(
+                'image' => $image
+            ),
+            array(
+                'image_add' => new Repository_Image_Add
+            ),
+            array(
+                'photoshopper' => new Tool_Photoshopper,
+                'validator' => new Tool_Validator
+            )
+        );
+    }
+
+    private function extract_posted_file_into_file_data($post_name)
+    {
+        $file = new Cavis\Core\Data\File;
+        $file->name = $_FILES[$post_name]['name'];
+        $file->tmp_name = $_FILES[$post_name]['tmp_name'];
+        $file->mimetype = $_FILES[$post_name]['type'];
+        $file->filesize_in_bytes = $_FILES[$post_name]['size'];
+        $file->error_code = $_FILES[$post_name]['error'];
+        return $file;
     }
 }
--- application/classes/View/Project/Submit.php
+++ application/classes/View/Project/Submit.php
@@ -5,4 +5,102 @@
  */
 
 class View_Project_Submit extends View_Layout
-{}
+{
+    public $errors;
+
+    public function has_errors()
+    {
+        $session = Session::instance();
+        $this->errors = $session->get_once('errors');
+
+        if ( ! empty($this->errors))
+            return TRUE;
+        else
+            return FALSE;
+    }
+
+    public function errors()
+    {
+        $errors = array();
+
+        foreach ($this->errors as $error)
+        {
+            if ($error === 'name')
+            {
+                $errors[] = array('error' => 'Make sure you have a project title between 1-60 letters long.');
+            }
+            elseif ($error === 'author')
+            {
+                $errors[] = array('error' => 'You must provide your full name between 1-60 letters long.');
+            }
+            elseif ($error === 'website')
+            {
+                $errors[] = array('error' => 'You need to provide a valid website url, such as http://google.com/');
+            }
+            elseif ($error === 'email')
+            {
+                $errors[] = array('error' => 'Your email address does not seem valid.');
+            }
+            elseif ($error === 'summary')
+            {
+                $errors[] = array('error' => 'You need to provide a project summary.');
+            }
+            elseif ($error === 'description')
+            {
+                $errors[] = array('error' => 'You need to provide a detailed project description.');
+            }
+            elseif ($error === 'file')
+            {
+                $errors[] = array('error' => 'Make sure you provide a main image that is less than 3MB, JPG or PNG, and at least 1200px in width.');
+            }
+            elseif ($error === 'supplementary_file')
+            {
+                $errors[] = array('error' => 'Make sure your supplementary images are less than 3MB, JPG or PNG, and at least 600px in width.');
+            }
+        }
+
+        return $errors;
+    }
+
+    public function categories()
+    {
+        $selected_category = (isset($this->category)) ? $this->category : NULL;
+        $categories = array(
+            array(
+                'id' => 6,
+                'name' => 'Flinders St Station - Ross Anderson',
+                'is_selected' => FALSE
+            ),
+            array(
+                'id' => 2,
+                'name' => 'Urban Heterodoxy - Michael Zanardo',
+                'is_selected' => FALSE
+            ),
+            array(
+                'id' => 3,
+                'name' => 'The Intimate Metropolis - Stephen Collier',
+                'is_selected' => FALSE
+            ),
+            array(
+                'id' => 4,
+                'name' => 'National Centre for Cultural Competence - Michael Tawa',
+                'is_selected' => FALSE
+            ),
+            array(
+                'id' => 5,
+                'name' => 'Beach City - Dagmar Reinhardt',
+                'is_selected' => FALSE
+            ),
+        );
+
+        foreach ($categories as $key => $category)
+        {
+            if ($category['id'] == $selected_category)
+            {
+                $categories[$key]['is_selected'] = TRUE;
+            }
+        }
+
+        return $categories;
+    }
+}
