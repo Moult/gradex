8cac5e78c843ee8ca88853ef6d4d58d7ecd1d42b
Dion Moult <dion@thinkmoult.com>
2013-11-10 18:09:27 +1100

    Impelement rest of repository

--- application/classes/Repository/Image/Add.php
+++ application/classes/Repository/Image/Add.php
@@ -19,9 +19,41 @@ class Repository_Image_Add implements Cavis\Core\Usecase\Image\Add\Repository
     /**
      * @return int The unique id of the saved image
      */
-    public function save_image($name, $thumbnail_path, $file_path, $supplementary_file_paths, $category_id)
+    public function save_image($name, $author, $website, $email, $summary, $description, $thumbnail_path, $file_path, $supplementary_file_paths, $category_id)
     {
-
+        for ($i = 0; $i < 3; $i++) {
+            if ( ! isset($supplementary_file_paths[$i]))
+            {
+                $supplementary_file_paths[$i] = '';
+            }
+        }
+        DB::insert('projects', array(
+                'name',
+                'author',
+                'website',
+                'email',
+                'summary',
+                'description',
+                'thumbnail',
+                'file',
+                'supplementary_file_1',
+                'supplementary_file_2',
+                'supplementary_file_3',
+                'category'))
+            ->values(array(
+                $name,
+                $author,
+                $website,
+                $email,
+                $summary,
+                $description,
+                $thumbnail_path,
+                $file_path,
+                $supplementary_file_paths[0],
+                $supplementary_file_paths[1],
+                $supplementary_file_paths[2],
+                $category_id))
+            ->execute();
     }
 
     /**
@@ -29,6 +61,21 @@ class Repository_Image_Add implements Cavis\Core\Usecase\Image\Add\Repository
      */
     public function save_file(Cavis\Core\Data\File $file)
     {
+        $config = Kohana::$config->load('upload');
 
+        if ( ! empty($file->full_path))
+        {
+            $dest_path = $config->get('directory').uniqid().'.png';
+            rename($file->full_path, $dest_path);
+            return $dest_path;
+        }
+
+        return Upload::save(array(
+            'name' => $file->name,
+            'type' => $file->mimetype,
+            'tmp_name' => $file->tmp_name,
+            'error' => $file->error_code,
+            'size' => $file->filesize_in_bytes
+        ), uniqid().'_'.$file->name, $config->get('directory'));
     }
 }
