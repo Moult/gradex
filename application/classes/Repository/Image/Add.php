b84fadcbc3ea6e58e0c0df16ade559500a18428d
Dion Moult <dion@thinkmoult.com>
2013-11-10 15:39:38 +1100

    Placeholder repository implementation

---
+++ application/classes/Repository/Image/Add.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+class Repository_Image_Add implements Cavis\Core\Usecase\Image\Add\Repository
+{
+    public function does_category_exist($id)
+    {
+
+    }
+
+    /**
+     * @return int The unique id of the saved image
+     */
+    public function save_image($name, $thumbnail_path, $file_path, $supplementary_file_paths, $category_id)
+    {
+
+    }
+
+    /**
+     * @return mixed string bool full path to uploaded file or FALSE if failed
+     */
+    public function save_file(Cavis\Core\Data\File $file)
+    {
+
+    }
+}
